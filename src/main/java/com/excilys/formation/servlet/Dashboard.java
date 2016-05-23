34cda510ea15ede53beb6881a5742d8a0e2b271a
Alexandre Chaouat <alexandre.chaouat@gmail.com>
2016-05-23 18:02:28 +0200

    MISC

--- src/main/java/com/excilys/formation/servlet/Dashboard.java
+++ src/main/java/com/excilys/formation/servlet/Dashboard.java
@@ -1,4 +1,4 @@
-package servlet;
+package com.excilys.formation.servlet;
 
 import java.io.IOException;
 import javax.servlet.ServletException;
@@ -14,7 +14,7 @@ import javax.servlet.http.HttpServletResponse;
 public class Dashboard extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-    /**
+    /**	
      * Default constructor. 
      */
     public Dashboard() {
@@ -25,8 +25,7 @@ public class Dashboard extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		request.getRequestDispatcher("dashboard.jsp").forward(request, response);
 	}
 
 	/**
