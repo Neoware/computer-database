920a23557e15ef8fda716b02475b1f3023232a51
Alexandre Chaouat <alexandre.chaouat@gmail.com>
2016-06-10 14:30:00 +0200

    test github hook

--- src/test/java/com/excilys/formation/selenium/SeleniumTest.java
+++ src/test/java/com/excilys/formation/selenium/SeleniumTest.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxProfile;
 
+//push purpose
 public class SeleniumTest {
 	private WebDriver driver;
 	private String baseUrl;
